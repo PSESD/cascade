833fe338756004b9efb041df8664c6e0c931b19f
Jacob Morrison <jmorrison@psesd.org>
2013-10-25 19:42:48 -0700

    with gii template

---
+++ app/gii/cascadeModule/form.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @var yii\base\View $this
+ * @var yii\widgets\ActiveForm $form
+ * @var yii\gii\generators\module\Generator $generator
+ */
+?>
+<div class="module-form">
+<?php
+//	echo $form->field($generator, 'moduleName');
+
+//	echo $form->field($generator, 'moduleClass');
+//	echo $form->field($generator, 'moduleID');
+
+	echo $form->field($generator, 'baseNamespace');
+	echo $form->field($generator, 'tableName');
+	echo $form->field($generator, 'modelClass');
+//	echo $form->field($generator, 'ns');
+//	echo $form->field($generator, 'baseClass');
+//	echo $form->field($generator, 'db');
+//	echo $form->field($generator, 'generateRelations')->checkbox();
+//	echo $form->field($generator, 'generateLabelsFromComments')->checkbox();
+
+?>
+</div>
