95d5308da29aa0677c4cb9fa65ea1ecba5b3106d
Jacob Morrison <jmorrison@psesd.org>
2013-11-14 16:16:06 -0800

    with widgets

--- app/components/web/widgets/base/SimpleList.php
+++ app/components/web/widgets/base/SimpleList.php
@@ -1,8 +1,24 @@
 <?php
 namespace app\components\web\widgets\base;
 
+use Yii;
+
+use \yii\bootstrap\Button;
+
+ini_set('memory_limit', -1);
 class SimpleList extends BaseList {
 	public function renderContent() {
-		
+		return get_class($this->owner);
+	}
+
+	public function getHeaderMenu() {
+		$menu = [];
+		if (Yii::$app->gk->canGeneral('create', $this->owner->primaryModel)) {
+			$menu[] = [
+				'label' => 'Create',
+				'url' => '#'
+			];
+		}
+		return $menu;
 	}
 }
