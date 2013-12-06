f11fe8cccadd02727d6854c9c421ce60642fe6a7
Jacob Morrison <jmorrison@psesd.org>
2013-12-05 16:04:44 -0800

    bad env

--- src/cascade/config/environments/setups/development/collectors.php
+++
@@ -1,4 +0,0 @@
-<?php
-$parent = dirname(__FILE__) . DIRECTORY_SEPARATOR . '..'. DIRECTORY_SEPARATOR . '..'. DIRECTORY_SEPARATOR . 'common' . DIRECTORY_SEPARATOR . basename(__FILE__);
-return array_merge(include($parent), []);
-?>
