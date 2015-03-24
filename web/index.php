6882426fa5a584ac5431501f5c592dafa3d981b7
Jake Oehler Morrison <jmorrison@psesd.org>
2015-03-24 13:01:55 -0700

    git update, init update

--- web/index.php
+++ web/index.php
@@ -1,6 +1,5 @@
 <?php
 require(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
-require(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'yiisoft'. DIRECTORY_SEPARATOR . 'yii2'. DIRECTORY_SEPARATOR . 'Yii.php');
 if (!class_exists('canis\base\ApplicationEngine')) {
 	throw new \Exception('Canis core libraries have not been installed. Have you ran `composer install`?');
 }
--- web/setup.php
+++ web/setup.php
@@ -1,7 +1,6 @@
 <?php
 define('CANIS_SETUP', true);
 require(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
-require(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'yiisoft'. DIRECTORY_SEPARATOR . 'yii2'. DIRECTORY_SEPARATOR . 'Yii.php');
 if (!class_exists('canis\base\ApplicationEngine')) {
 	throw new \Exception('Canis core libraries have not been installed. Have you ran `composer install`?');
 }
